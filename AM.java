3ac5259ccb2d35d3b2c2f43d4cf34f1784c0c897
STYLIANI PANOPOULOU <t8230111@aueb.gr>
2024-11-11 14:42:13 +0000

    Stella added her AM

--- AM.java
+++ AM.java
@@ -2,5 +2,6 @@ public class AM {
     public static void main(String[] args) {
         System.out.println("8230005");
         System.out.println("8230061");
+        System.err.println("8230111");
     }
 }
